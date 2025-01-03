4eb3baa02ab506101ba91a228ec046d934ee3153
Menichetti <menichetti.dev@gmail.com>
2025-01-02 21:43:29 -0300

    cargar pago page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegaloPageComponent } from './pages/regalo-page/regalo-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { guardsGuard } from './guards.guard'; // Importa el guard
 import { CargarRutinaComponent } from './pages/cargar-rutina/cargar-rutina.component';
+import { CargarPagoPageComponent } from './pages/cargar-pago-page/cargar-pago-page.component';
 
 
 export const routes: Routes = [
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: 'rutina', component: RutinaPageComponent, canActivate: [guardsGuard] },
     { path: 'cargar-rutina', component: CargarRutinaComponent, canActivate: [guardsGuard] },
+    { path: 'cargar-pago', component: CargarPagoPageComponent, canActivate: [guardsGuard] },
     { path: 'regalo', component: RegaloPageComponent },
     {path: '**', component: HomePageComponent}
 
