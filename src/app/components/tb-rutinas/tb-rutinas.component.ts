09c189ea248326f3be416cb1349b60a65f275d72
MenichettiDev <menichetti.dev@gmail.com>
2025-02-25 22:43:45 -0300

    mover visor de rutinas a components

--- src/app/components/tb-rutinas/tb-rutinas.component.ts
+++ src/app/components/tb-rutinas/tb-rutinas.component.ts
@@ -3,10 +3,13 @@ import { RutinasService } from '../../service/rutinas.service';
 import { plan, ejercicioRutina,ejerciciosPorDia,rutinaArmada } from '../../data/interfaces/rutinaArmadaInterface';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { VisorRutinaComponent } from '../../pages/03-rutinas/mis-rutinas/visor-rutina/visor-rutina.component';
 
 @Component({
   selector: 'app-tb-rutinas',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './tb-rutinas.component.html',
   styleUrl: './tb-rutinas.component.css'
 })
@@ -14,7 +17,7 @@ export class TbRutinasComponent implements OnInit {
   @Input() idAtleta: number | null = null; // Recibe el ID del atleta
   rutinas: rutinaArmada[] = []; // Lista de rutinas
 
-  constructor(private rutinaService: RutinasService) {}
+  constructor(private rutinaService: RutinasService, private router: Router, private dialog : MatDialog) {}
 
   ngOnInit(): void {
     if (this.idAtleta) {
@@ -31,5 +34,11 @@ export class TbRutinasComponent implements OnInit {
         console.error('Error al cargar rutinas', error);
       }
     );
+  }
+
+  verDetallesRutina(rutina: rutinaArmada): void {
+    this.dialog.open(VisorRutinaComponent, {
+      data: rutina,
+    });
   }
 }
