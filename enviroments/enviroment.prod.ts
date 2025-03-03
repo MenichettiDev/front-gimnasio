4c14eaaf7fb0c491b25148ceffb3fa1c936c35e6
MenichettiDev <menichetti.dev@gmail.com>
2025-03-03 16:56:45 -0300

    probando variables de entorno

---
+++ enviroments/enviroment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://gymrats.com.ar:7000', // URL para producción
+};
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from './auth/auth.service';
+import { environment } from '../../../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiUrl = environment.apiUrl; // Usa la URL del entorno
 
   constructor(private http: HttpClient, private authService: AuthService, private router: Router) { }
 
   login(usuario: string, contrasenia: string) {
-    return this.http.post('http://localhost:7000/login', { usuario, contrasenia })
+    return this.http.post(`${this.apiUrl}/login`, { usuario, contrasenia })
       .subscribe(
         (response) => {
           this.authService.saveUser(response);
